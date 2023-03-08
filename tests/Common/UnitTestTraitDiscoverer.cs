32a9673e15ede2a64069aff21e8dc25b723e5be4
Patrick Mathias <Patrick.Mathias@vlh.de>
2023-03-08 13:13:01 +0100

    Add attribute to group Unit-Tests by traits

---
+++ tests/Common/UnitTestTraitDiscoverer.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTestTraitDiscoverer.cs" company="Vereinigte Lohnsteuerhilfe e.V">
+// copyright (c) 2022 Vereinigte Lohnsteuerhilfe e.V
+//               Fritz-Voigt-Str. 13
+//               67433 Neustadt/Wstr.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace SimpleOAuth2Client.AspNetCore.UnitTests.Common
+{
+    /// <summary>
+    /// This is a TraitDiscoverer implementation. Discover all Tests marked by the "UnitTest" attribute.
+    /// </summary>
+    [SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "This class is used by Xunit Testframwork")]
+    internal sealed class UnitTestTraitDiscoverer : ITraitDiscoverer
+    {
+        private const string KEY = "Category";
+        private const string VALUE = "UnitTest";
+
+        /// <inheritdoc/>
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            yield return new KeyValuePair<string, string>(KEY, VALUE);
+        }
+    }
+}
--- tests/ExampleUnitTest.cs
+++ tests/ExampleUnitTest.cs
@@ -1,9 +1,11 @@
+using SimpleOAuth2Client.AspNetCore.UnitTests.Common;
 using Xunit;
 
 namespace SimpleOAuth2Client.AspNetCore.UnitTests;
 
-public class UnitTest1
+public class ExampleUnitTest
 {
+    [UnitTest]
     [Fact]
     public void Given_When_Then()
     {
