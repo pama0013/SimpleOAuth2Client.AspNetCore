c286bc35cab91cf36dc56a182da64ac72c6e9f25
Patrick Mathias <Patrick.Mathias@vlh.de>
2023-03-10 09:19:30 +0100

    Store the credentials for the example inside the configuration

--- examples/SimpleOAuth2Client.AspNetCore.Example/Program.cs
+++ examples/SimpleOAuth2Client.AspNetCore.Example/Program.cs
@@ -1,22 +1,23 @@
 using CSharpFunctionalExtensions;
 using SimpleOAuth2Client.AspNetCore.Common.Errors;
 using SimpleOAuth2Client.AspNetCore.Contracts;
+using SimpleOAuth2Client.AspNetCore.Example.Options;
 using SimpleOAuth2Client.AspNetCore.Extensions;
 using SimpleOAuth2Client.AspNetCore.Model;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Pass the SimpleOAuth2ClientOptions as command line arguments or environment variables
-//IConfigurationSection section = builder.Configuration.GetSection("SimpleOAuth2ClientOptions");
-//SimpleOAuth2ClientOptions? simpleOAuth2ClientOptions = section
-//    .Get<SimpleOAuth2ClientOptions>()
-//    ?? throw new InvalidOperationException("Could not load SimpleOAuth2ClientOptions.");
+IConfigurationSection section = builder.Configuration.GetSection("SimpleOAuth2ClientOptions");
+SimpleOAuth2ClientOptions? simpleOAuth2ClientOptions = section
+    .Get<SimpleOAuth2ClientOptions>()
+    ?? throw new InvalidOperationException("Could not load SimpleOAuth2ClientOptions.");
 
 builder.Services.AddSimpleOAuth2Client(options =>
 {
-    options.ClientId = "vlhVereinsprogramm";
-    options.ClientSecret = "password";
-    options.TokenEndpoint = new Uri("http://localhost:8080/auth/token");
+    options.ClientId = simpleOAuth2ClientOptions.ClientId;
+    options.ClientSecret = simpleOAuth2ClientOptions.ClientSecret;
+    options.TokenEndpoint = simpleOAuth2ClientOptions.TokenEndpoint;
 });
 
 WebApplication app = builder.Build();
